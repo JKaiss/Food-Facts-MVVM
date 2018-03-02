2c2bc598c1247613207c3cb35c0c136859ce7c88
JKaiss <jaafoura.kais@gmail.com>
2018-03-02 15:35:54 +0100

    add leakCanary

--- app/src/main/java/com/jaafoura/foodfacts/BaseApplication.java
+++ app/src/main/java/com/jaafoura/foodfacts/BaseApplication.java
@@ -5,6 +5,7 @@ import com.jaafoura.foodfacts.dagger.AppComponent;
 import com.jaafoura.foodfacts.dagger.AppModule;
 import com.jaafoura.foodfacts.dagger.DaggerAppComponent;
 import com.jaafoura.foodfacts.dagger.RoomModule;
+import com.squareup.leakcanary.LeakCanary;
 
 /**
  * Created by kjaafoura on 26/10/2017.
@@ -17,6 +18,12 @@ public class BaseApplication extends Application {
   @Override
   public void onCreate() {
     super.onCreate();
+    if (LeakCanary.isInAnalyzerProcess(this)) {
+      // This process is dedicated to LeakCanary for heap analysis.
+      // You should not init your app in this process.
+      return;
+    }
+    LeakCanary.install(this);
 
     this.appComponent = DaggerAppComponent.builder()
         .appModule(new AppModule(this))
